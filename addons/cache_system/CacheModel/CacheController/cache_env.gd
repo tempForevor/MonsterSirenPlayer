423d214d43a7af52bf70691f51c5d65dc3f76a52
tempForevor <e-mjcsmail@qq.com>
2026-06-22 22:43:24 +0800

    fix failed to create file when special characters exist in file name

--- addons/cache_system/CacheModel/CacheController/cache_env.gd
+++ addons/cache_system/CacheModel/CacheController/cache_env.gd
@@ -8,8 +8,26 @@ var cache_dir : String = "user://Cache/"
 func _init(v_cache_dir:String="user://Cache/") -> void:
 	cache_dir = v_cache_dir
 
+var replaced_file_name_char : Dictionary[String,String] = {
+	"<":"-[lcb]-",
+	">":"-[rcb]-",
+	#":":"：", # This may need to be specially judged someday
+	"\"":"“",
+	#"/":"-[rrl]-",
+	#"\\":"-[lrl]-",
+	"|":"-[mlm]-",
+	"*":"-[mul]-",
+	"?":"？"
+}
+
+func avaliable_file_name(raw:String)->String:
+	var res = raw
+	for i in replaced_file_name_char.keys():
+		res = res.replace(i,replaced_file_name_char[i])
+	return res
+
 func get_item_path(item:String) -> String:
-	return cache_dir.path_join(item)
+	return avaliable_file_name(cache_dir.path_join(item))
 
 func generate_item_with_type(type:String,name:String) -> String:
 	return type.path_join(name)
@@ -18,27 +36,29 @@ func generate_item(res,name:String) -> String:
 	return generate_item_with_type(res.get_class(),name)
 
 func test_path_exists(path:String,is_dir:bool=false) -> bool:
+	var tpath = avaliable_file_name(path)
 	var dir = DirAccess.open(cache_parent)
-	if not dir.dir_exists(path.get_base_dir()):
+	if not dir.dir_exists(tpath.get_base_dir()):
 		return false
 	if is_dir:
 		return true
-	return dir.file_exists(path)
+	return dir.file_exists(tpath)
 
 func test_item_existe(item:String,is_dir:bool=false) -> bool:
 	return test_path_exists(get_item_path(item),is_dir)
 
 func create_path_if_miss(path:String,is_dir:bool=false) -> Error:
+	var tpath = avaliable_file_name(path)
 	var dir := DirAccess.open(cache_parent)
 	var error := OK
-	if not dir.dir_exists(path.get_base_dir()):
-		error = dir.make_dir_recursive(path.get_base_dir())
-	print("Now error : ",error)
+	if not dir.dir_exists(tpath.get_base_dir()):
+		error = dir.make_dir_recursive(tpath.get_base_dir())
+	#print("Now error : ",error)
 	if is_dir:
 		return error
-	if dir.file_exists(path):
+	if dir.file_exists(tpath):
 		return ERR_ALREADY_EXISTS
-	var file := FileAccess.open(path,FileAccess.WRITE_READ)
+	var file := FileAccess.open(tpath,FileAccess.WRITE_READ)
 	error = FileAccess.get_open_error()
 	if file == null:
 		error = ERR_FILE_UNRECOGNIZED
@@ -48,12 +68,13 @@ func create_item_if_miss(item:String,is_dir:bool=false) -> Error:
 	return create_path_if_miss(get_item_path(item),is_dir)
 
 func remove_path_if_exists(path:String) -> Error:
+	var tpath = avaliable_file_name(path)
 	var dir = DirAccess.open(cache_parent)
-	if not dir.dir_exists(path.get_base_dir()):
+	if not dir.dir_exists(tpath.get_base_dir()):
 		return ERR_FILE_NOT_FOUND
-	if not dir.file_exists(path):
+	if not dir.file_exists(tpath):
 		return ERR_FILE_NOT_FOUND
-	return dir.remove(path)
+	return dir.remove(tpath)
 
 func remove_item_if_exists(item:String) -> Error:
 	return remove_path_if_exists(get_item_path(item))
